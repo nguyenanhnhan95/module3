283a4909145548f67cbf0bad214d74026f5b04ab
nguyenanhnhan95 <nguyenanhnhan95@gmail.com>
2023-05-16 10:43:37 +0700

    Em nop bai tap

--- ss2_design_create_data/bai_tap/manage_sales/manage_sales.sql
+++ ss2_design_create_data/bai_tap/manage_sales/manage_sales.sql
@@ -5,7 +5,7 @@ cID int primary key auto_increment,
 cName varchar(45),
 cAge int
 );
-create table OrderProduct(
+create table order_product(
 oID int primary key auto_increment,
 cID int,
 foreign key(cID) references customer(cID),
@@ -18,10 +18,11 @@ pName varchar(45) not null,
 pPrice double not null
 );
 
-create table OrderDetail(
+create table order_detail(
 oID int ,
 pID int ,
 primary key(oID,pID),
 foreign key(pID) references product(pID),
+foreign key(oID) references order_product(oID),
 odQTY varchar(45) not null
 )
